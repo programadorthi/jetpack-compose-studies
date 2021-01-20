52c19ea19501b11d372eb79cc236f7e7d0a017f1
Thiago Santos <programadorthi@gmail.com>
2021-01-20 15:05:03 -0300

    Initial commit with episodes 001-003

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # jetpack-compose-studies
-Project used in lives to study Jetpack Compose
+Project to study Jetpack Compose with subscribers on Twitch lives.
+
+# Things we had discovered in our study sections
+1. Modifiers order is very important. Changing the order can create unexpected behaviors;
+2. `RectangleShape` is the default shape in some modifiers;
+3. By default compose nodes consumes minimum space as possible. If you needs increase width/height you have to do that using a modifier as `fillMaxSize`;
+4. Applying `border` modifier also do a `clip`. So we don't need to apply a clip before apply a border;
+5. Modifiers can be combined using `then()` or operator `+` creating a chain of modifiers. This is a good expansion between public and private applied modifiers.
+6. There is two ways to load an image resource. 
+  - `imageResource` load image in a synchronous way. Easy to use.
+  - `loadImageResource` is the asynchronous version. More verbose.
+7. 
---
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                Surface(
+                        modifier = Modifier.fillMaxSize()
+                ) {
+                    ContentView()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ContentView() {
+    Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // TODO: Replace Image with Google Maps using AndroidView
+        Image(
+                bitmap = imageResource(id = R.drawable.passarela),
+                modifier = Modifier.fillMaxWidth()
+        )
+        CircleImage(
+                modifier = Modifier
+                        .offset(y = (-75).dp)
+        )
+        Column(
+                modifier = Modifier.padding(8.dp)
+        ) {
+            Text(
+                    style = MaterialTheme.typography.h4,
+                    text = "Turtle Rock"
+            )
+            Row {
+                Text(
+                        style = MaterialTheme.typography.subtitle1,
+                        text = "Joshua Tree National Park"
+                )
+                Spacer(modifier = Modifier.weight(weight = 1f))
+                Text(
+                        style = MaterialTheme.typography.subtitle1,
+                        text = "California"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CircleImage(modifier: Modifier = Modifier) {
+    Image(
+            bitmap = imageResource(id = R.drawable.turtlerock),
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                    .size(150.dp)
+                    .border(width = 4.dp, color = Color.White, shape = CircleShape)
+                    .shadow(elevation = 7.dp, shape = CircleShape)
+            //.then(modifier)
+    )
+    /*val image = loadImageResource(id = R.drawable.turtlerock)
+    image.resource.resource?.let {
+        Surface(color = Color.Blue) {
+            Image(
+                    bitmap = it,
+                    modifier = Modifier
+                            .offset(x = 20.dp, y = 20.dp)
+                            //.preferredSize(120.dp)
+                            //.clip(shape = CircleShape)
+                            .border(width = 4.dp, color = Color.Gray, shape = CircleShape)
+                            .shadow(elevation = 10.dp, shape = CircleShape)
+            )
+        }
+    }*/
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ContentView()
+}
