4402f5e347b54526079f72506e5416f51980b255
Thiago Santos <programadorthi@gmail.com>
2021-01-30 18:53:25 -0300

    added compose navigation

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -11,8 +12,6 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -23,10 +22,20 @@ import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.example.jetpackcompose.domain.Landmark
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
+
+object Destinations {
+    const val DETAILS = "details"
+    const val LANDMARKS = "landmarks"
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +53,19 @@ class MainActivity : AppCompatActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    ContentView(landmarks)
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Destinations.LANDMARKS,
+                        builder = {
+                            composable(Destinations.LANDMARKS) {
+                                ContentView(navController, landmarks)
+                            }
+                            composable(Destinations.DETAILS) {
+                                DetailsView(navController)
+                            }
+                        }
+                    )
                 }
             }
         }
@@ -52,20 +73,29 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun ContentView(landmarks: List<Landmark>) {
-    LazyColumn {
-        items(count = landmarks.size) { index ->
-            val landmark = landmarks[index]
-            LandmarkRow(landmark)
-            if (index != landmarks.lastIndex) {
-                Divider()
+fun ContentView(navController: NavController, landmarks: List<Landmark>) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "Landmarks",
+            style = MaterialTheme.typography.h4
+        )
+        LazyColumn(modifier = Modifier.fillMaxHeight()) {
+            items(count = landmarks.size) { index ->
+                val landmark = landmarks[index]
+                LandmarkRow(landmark, modifier = Modifier.clickable {
+                    navController.navigate(Destinations.DETAILS)
+                })
+                if (index != landmarks.lastIndex) {
+                    Divider()
+                }
             }
         }
     }
 }
 
 @Composable
-fun DetailsView() {
+fun DetailsView(navController: NavController) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -150,12 +180,12 @@ fun CircleImage(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun LandmarkRow(landmark: Landmark) {
+fun LandmarkRow(landmark: Landmark, modifier: Modifier = Modifier) {
     val imageId = AmbientContext.current.run {
         resources.getIdentifier(landmark.imageName, "drawable", packageName)
     }
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
@@ -175,5 +205,5 @@ fun LandmarkRow(landmark: Landmark) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ContentView(listOf())
+    //ContentView(navController, listOf())
 }
