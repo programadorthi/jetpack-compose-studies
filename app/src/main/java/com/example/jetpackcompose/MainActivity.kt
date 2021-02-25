f66db127c38b6d1531d77be182d8814c75b5ec02
Thiago Santos <programadorthi@gmail.com>
2021-02-24 22:24:14 -0300

    Upgrade to compose Beta

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,33 +1,55 @@
 package com.example.jetpackcompose
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.AmbientContext
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.example.jetpackcompose.domain.Landmark
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
-import kotlinx.serialization.builtins.ListSerializer
-import kotlinx.serialization.json.Json
-import kotlin.random.Random
 
 const val LANDMARK_ID = "landmark_id"
 
@@ -130,7 +152,7 @@ fun DetailsView(landmark: Landmark?) {
             Box {
                 // TODO: Replace Image with Google Maps using AndroidView
                 Image(
-                    bitmap = imageResource(id = R.drawable.passarela),
+                    painter = painterResource(id = R.drawable.passarela),
                     contentDescription = landmark?.name ?: "Isso aqui e uma imagem",
                     modifier = Modifier.fillMaxWidth()
                 )
@@ -187,11 +209,11 @@ fun DetailsView(landmark: Landmark?) {
 
 @Composable
 fun CircleImage(landmark: Landmark, modifier: Modifier = Modifier) {
-    val imageId = AmbientContext.current.run {
+    val imageId = LocalContext.current.run {
         resources.getIdentifier(landmark.imageName, "drawable", packageName)
     }
     Image(
-        bitmap = imageResource(id = imageId),
+        painter = painterResource(id = imageId),
         contentDescription = landmark.name,
         contentScale = ContentScale.Crop,
         modifier = modifier
@@ -218,7 +240,7 @@ fun CircleImage(landmark: Landmark, modifier: Modifier = Modifier) {
 
 @Composable
 fun LandmarkRow(landmark: Landmark, modifier: Modifier = Modifier) {
-    val imageId = AmbientContext.current.run {
+    val imageId = LocalContext.current.run {
         resources.getIdentifier(landmark.imageName, "drawable", packageName)
     }
     Row(
@@ -228,7 +250,7 @@ fun LandmarkRow(landmark: Landmark, modifier: Modifier = Modifier) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
-            bitmap = imageResource(id = imageId),
+            painter = painterResource(id = imageId),
             contentDescription = landmark.name,
             modifier = Modifier.size(50.dp)
         )
